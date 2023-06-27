a16019594e462dab069d53b8a24b77e255fb3d6c
Maria Perez Cobo <mariaperezcobo@hotmail.com>
2023-06-27 15:40:25 -0300

    agregue header: navbar y logo carrito

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+export const Header = () =>{
+    return (
+        <div>
+           <div className="color--barranav">
+           <nav className="color--barranav navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"></a>
+                    <img className="logo" src="./imagenes/logo2.png" alt="logo"></img>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav> 
+           </div>
+           <div className="encabezado">
+            
+           <button type="button" class="btn position-relative contenedor_logoCarrito">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg>  
+                <a class="carritoSubtitulo" href="./index.html#tusCompras"> Carrito</a> 
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill badge rounded-pill colorCirculo">
+                5
+                <span class="visually-hidden">unread messages</span>
+                </span>
+            </button>
+           </div>
+           
+        </div>
+    )
+}
