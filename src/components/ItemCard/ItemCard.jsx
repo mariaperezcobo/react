68f9ae94dc8bdc15f58749b3c380402cbb5f004f
Maria Perez Cobo <mariaperezcobo@hotmail.com>
2023-07-12 08:52:58 -0300

    mejoras

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,21 +1,23 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card'; 
-
+import './ItemCard.scss'
 
 
 export const ItemCard = ({item}) => {
     return (
-        <div key={item.id} className='col-3 margin'>
-            <Card style={{ width: '20rem' }} >
+        <div  className='col-3 margin'>
+   <Card style={{ width: '20rem' }} >
            <Card.Img variant='top' src={item.imagen} />
             <Card.Body>
                <Card.Title className='titulo_card'>{item.nombre}</Card.Title>
                <Card.Text>
                  ${item.precio}
                </Card.Text>
-               <Button variant='primary'>Agregar al carrito</Button>
+               <Button className='boton_agregar' variant='secondary' >Agregar al carrito</Button>
            </Card.Body>
        </Card>
+
+
    </div>
     )
 }
